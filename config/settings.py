d00d5285b8928cf78e82b11b458af03d464fdc15
Pranavv157 <pranavsshinde157@gmail.com>
2026-03-27 18:56:59 +0530

    setttings and requirements issue fixed

--- config/settings.py
+++ config/settings.py
@@ -89,22 +89,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 import os
 import dj_database_url
 
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+ALLOWED_HOSTS = ["*"]
+
 DATABASES = {}
 
 if os.environ.get("DATABASE_URL"):
-    # Production (Render / Postgres)
     DATABASES["default"] = dj_database_url.parse(
         os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=False,
     )
 else:
-    # Local development (SQLite)
     DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
