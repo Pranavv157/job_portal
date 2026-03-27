bb352a995abd1c394f7b58e8074de6b1b67596d0
Pranavv157 <pranavsshinde157@gmail.com>
2026-03-27 19:14:23 +0530

    fix csrf issue

--- config/settings.py
+++ config/settings.py
@@ -154,5 +154,12 @@ LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app"
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
