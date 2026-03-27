5d5e840671773018c3b9b4f8f91211edc65a1447
Pranavv157 <pranavsshinde157@gmail.com>
2026-03-27 19:31:44 +0530

    relax password validation form

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,3 +6,7 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'role', 'password1', 'password2']
+
+    def clean_password2(self):
+        password = self.cleaned_data.get("password2")
+        return password
